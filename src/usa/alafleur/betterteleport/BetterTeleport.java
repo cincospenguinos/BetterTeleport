a18a97837e3b608d81c09ece89d56ae8a195f9a2
Andre LaFleur <andre.lafleur@utah.edu>
2016-09-02 13:11:34 -0600

    Move plugin.yml

--- src/usa/alafleur/betterteleport/BetterTeleport.java
+++ src/usa/alafleur/betterteleport/BetterTeleport.java
@@ -3,6 +3,8 @@ package usa.alafleur.betterteleport;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 /**
  * Better teleport command for Minecraft, powered by Spigot API.
  *
@@ -11,12 +13,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class BetterTeleport extends JavaPlugin {
 
     @Override
-    public void onEnable(){
+    public void onEnable() {
         // TODO: Load up database information
+
+        Logger.getLogger("Minecraft").info("[BetterTeleport] Enabling BetterTeleport");
     }
 
     @Override
     public void onDisable(){
         //Fired when the server stops and disables all plugins
+
+        Logger.getLogger("Minecraft").info("[BetterTeleport] Disabling BetterTeleport");
+    }
+
+    public static void logInfo(String message){
+        Logger.getLogger("Minecraft").info("[BetterTeleport] " + message);
     }
 }
