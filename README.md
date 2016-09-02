c1d877ebee8f4f652d67e6495590aa8a72ac3334
Andre LaFleur <andre.lafleur@utah.edu>
2016-09-02 11:50:06 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Better Teleport
+
+A better teleport command for minecraft. Intended features include:
+
+* Manage permissions for use of the teleport command
+* Ability to add, remove and query locations stored on a database
+* Ability to teleport to locations given an alias
+
+Because this will be a server plugin, all of these features will be included in game. You won't have to have a separate
+webpage full of that information; it will all be included in the UI.
---
+++ src/usa/alafleur/betterteleport/BetterTeleport.java
@@ -0,0 +1,22 @@
+package usa.alafleur.betterteleport;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Better teleport command for Minecraft, powered by Spigot API.
+ *
+ * @author Andre LaFleur
+ */
+public class BetterTeleport extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        //Fired when the server enables the plugin
+    }
+
+    @Override
+    public void onDisable(){
+        //Fired when the server stops and disables all plugins
+    }
+}
